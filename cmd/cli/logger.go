02549b6e5936a1c7c44d165614c61abbb3de9366
Zachary Schneider <z@platform.engineering>
2026-04-02 14:54:48 -0400

    fix: default log level to warn

--- cmd/cli/logger.go
+++ cmd/cli/logger.go
@@ -12,13 +12,11 @@ func LoggerFromFlag(level string, _ error) {
 	switch level {
 	case "ERR":
 		Logger.SetLevel(log.ErrorLevel)
-	case "WARN":
-		Logger.SetLevel(log.WarnLevel)
 	case "INFO":
 		Logger.SetLevel(log.InfoLevel)
 	case "DEBUG":
 		Logger.SetLevel(log.DebugLevel)
 	default:
-		Logger.SetLevel(log.FatalLevel)
+		Logger.SetLevel(log.WarnLevel)
 	}
 }
